e58a048655b250f1c54205d99ffa3221d8717716
ZMTakriti <71720717+ZMTakriti@users.noreply.github.com>
2020-12-28 20:34:45 +0300

    UI Changes

---
+++ MustacheGame/UI/SceneChanger.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+onready var animation = $Contol/AnimationPlayer
+var scene : String
+
+func change_scene(new_scene, anim):
+	scene = new_scene
+	animation.play(anim)
+	
+func _new_scene():
+	get_tree().change_scene(scene)
