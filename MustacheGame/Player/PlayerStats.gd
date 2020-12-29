492888334cc70ab1fc582c9083ffab1434bb5d38
ZMTakriti <71720717+ZMTakriti@users.noreply.github.com>
2020-12-29 03:55:17 +0300

    GameOver addition

---
+++ MustacheGame/Player/PlayerStats.gd
@@ -0,0 +1,9 @@
+extends "res://Stats.gd"
+
+func set_health(value):
+	health = value
+	emit_signal("health_changed", health)
+	if health <= 0:
+		emit_signal("no_health")
+		SceneChanger.change_scene("res://GameOver.tscn", 'fade')
+		health = 10
