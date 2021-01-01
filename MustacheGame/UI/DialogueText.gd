c3ad45a97976077151a383fc66575395b2c84d42
ZMTakriti <71720717+ZMTakriti@users.noreply.github.com>
2021-01-01 22:06:19 +0300

    DialogueBox bug fixes

--- MustacheGame/UI/DialogueText.gd
+++ MustacheGame/UI/DialogueText.gd
@@ -1,19 +1,22 @@
 extends RichTextLabel
 
-var dialogue = ["Hey, it's me, Goku!", "Hahaha!"]
+var dialogue = ["Yo test test test!", "Hey, it's me, Goku! Want to see me go beyond the level of a super saiyan?", "Hahaha!"]
 var page = 0
 
 func _ready():
 	set_bbcode(dialogue[page])
 	set_visible_characters(0)
+	set_process_input(true)
 
 func _input(event):
-	if event is InputEventMouseButton:
+	if Input.is_mouse_button_pressed(1):
 		if get_visible_characters() > get_total_character_count():
 			if page < dialogue.size() - 1:
 				page += 1
 				set_bbcode(dialogue[page])
 				set_visible_characters(0)
+		else:
+			set_visible_characters(get_total_character_count())
 
 func _on_Timer_timeout():
 	set_visible_characters(get_visible_characters() + 1)
